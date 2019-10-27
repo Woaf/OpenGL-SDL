5d5e4119ae2c24c04e95b2c9d76c38b8e329922f
Woaf <bfazekas@inf.elte.hu>
2019-10-27 13:27:47 +0100

    Industry standard code cleanup.

--- MiniGame/Application.cpp
+++ MiniGame/Application.cpp
@@ -2,35 +2,35 @@
 
 Woaf::Application* Woaf::Application::instance = nullptr;
 
-Woaf::Application::Application() : active(true) {
+Woaf::Application::Application () : active(true) {
 	try {
-		if (0 != SDL_Init(SDL_INIT_EVERYTHING)) {
-			throw ApplicationException("SDL initialisation error occured.", __LINE__, __FILE__);
+		if (0 != SDL_Init (SDL_INIT_EVERYTHING)) {
+			throw ApplicationException ("SDL initialisation error occured.", __LINE__, __FILE__);
 		}
 		else {
 
 		}
 	}
-	catch (ApplicationException& ex){
-		std::cerr << ex.what() << std::endl;
+	catch (ApplicationException& ex) {
+		std::cerr << ex.what () << std::endl;
 	}
 }
 
-Woaf::Application::~Application() {
+Woaf::Application::~Application () {
 
 }
 
-Woaf::Application* Woaf::Application::GetInstance() {
+Woaf::Application* Woaf::Application::GetInstance () {
 	if (nullptr == instance) {
-		instance = new Woaf::Application();
+		instance = new Woaf::Application ();
 	}
 	return instance;
 }
 
-bool Woaf::Application::Run() {
+bool Woaf::Application::Run () {
 	return active;
 }
 
-void Woaf::Application::Stop() {
+void Woaf::Application::Stop () {
 	active = false;
 }
--- MiniGame/Application.h
+++ MiniGame/Application.h
@@ -14,13 +14,15 @@ class GeneralException : public std::runtime_error
 {
 protected:
 public: 
-	explicit GeneralException(const std::string& error, size_t line = __LINE__, const char* file = __FILE__) : std::runtime_error(error + "\n\tat line: " + std::to_string(line) + "\n\tin file: " + file) {}
+	explicit GeneralException (const std::string& error, size_t line = __LINE__, const char* file = __FILE__) 
+		: std::runtime_error (error + "\n\tat line: " + std::to_string(line) + "\n\tin file: " + file) {}
 };
 
 class ApplicationException : public Woaf::GeneralException
 {	
 public:
-	explicit ApplicationException(const std::string& error, size_t line = __LINE__, const char* file = __FILE__) : GeneralException(error, line, file) {}
+	explicit ApplicationException (const std::string& error, size_t line = __LINE__, const char* file = __FILE__) 
+		: GeneralException (error, line, file) {}
 };
 
 class Application
@@ -29,13 +31,13 @@ private:
 	static Application* instance;
 	bool active;
 
-	explicit Application();
-	~Application();
+	explicit Application ();
+	~Application ();
 
 public:
-	static Application* GetInstance();
-	bool Run();
-	void Stop();
+	static Application* GetInstance ();
+	bool Run ();
+	void Stop ();
 };
 
 }

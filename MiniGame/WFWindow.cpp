59c883f5bb743db391dcb431bd8d0e1bdc9de822
Woaf <bfazekas@inf.elte.hu>
2019-10-27 14:56:11 +0100

    WFWindow and corresponding error classes created.

--- MiniGame/WFWindow.cpp
+++ MiniGame/WFWindow.cpp
@@ -1,1 +1,24 @@
 #include "WFWindow.h"
+
+Woaf::WFWindow::WFWindow (const std::string& title, size_t width, size_t height) {
+	mainWindow = std::make_unique<SDL_Window>(SDL_CreateWindow (title.c_str (), SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, width, height, WINDOW_FLAGS));
+
+	if (nullptr == mainWindow) {
+		throw WFWindowException (SDL_GetError (), __LINE__, __FILE__);
+	} 
+	else {
+
+	}
+}
+
+Woaf::WFWindow::WFWindow (const std::string& title, size_t x, size_t y, size_t width, size_t height) {
+	mainWindow = std::make_unique<SDL_Window> (SDL_CreateWindow (title.c_str (), x, y, width, height, WINDOW_FLAGS));
+	
+	if (nullptr == mainWindow) {
+		throw WFWindowException (SDL_GetError (), __LINE__, __FILE__);
+	}
+}
+
+Woaf::WFWindow::~WFWindow () {
+	SDL_Quit();
+}
--- MiniGame/WFWindow.h
+++ MiniGame/WFWindow.h
@@ -1,11 +1,31 @@
 #pragma once
 
+#include <memory>
+
+#include <SDL.h>
+
+#include "WFGeneralException.h"
+
 namespace Woaf
 {
 
+#define WINDOW_FLAGS SDL_WINDOW_SHOWN | SDL_WINDOW_OPENGL
+
+class WFWindowException : public WFGeneralException
+{
+public: 
+	WFWindowException(const std::string& error, size_t line, const char* file) : WFGeneralException(error, line, file) {}
+};
+
+
 class WFWindow
 {
-
+private: 
+	std::unique_ptr<SDL_Window> mainWindow;
+public: 
+	WFWindow(const std::string&, size_t, size_t);
+	WFWindow(const std::string&, size_t, size_t, size_t, size_t);
+	~WFWindow();
 };
 
 }
